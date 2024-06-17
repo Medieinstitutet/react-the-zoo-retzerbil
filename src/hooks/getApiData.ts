7231c3ffd07c4b15d1e933494d98e320d16b2fde
Andreas Retzius <andreasretzius@gmail.com>
2024-06-17 13:37:10 +0200

    🐛 getApiData.ts now returns the list of animals as IAnimal

--- src/hooks/getApiData.ts
+++ src/hooks/getApiData.ts
@@ -4,7 +4,7 @@ import { IAnimalResponse } from "../models/IAnimalResponse";
 export const getApiData = (url:string) =>{
     const get = async () =>{
         const response = await axios.get<IAnimalResponse>(url);
-        return response.data
+        return response.data.AnimalList
     }
     return {get}
 }
