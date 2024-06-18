6e652ce703ad621a0c5a56804e664a006f367632
Andreas Retzius <andreasretzius@gmail.com>
2024-06-18 15:02:44 +0200

    💄 ✨ Each animal is now an individual list item +  small SCSS change

--- src/components/Animal.tsx
+++ src/components/Animal.tsx
@@ -1,14 +1,13 @@
 import { IAnimal } from "../models/IAnimal"
-import "../AnimalStyling.scss"
 interface IAnimalProps{
     animal: IAnimal
 }
 export const Animal = ({animal}:IAnimalProps) =>{
-
-
     return <>
+    <li className="Animal">
     <img src={animal.imageUrl} alt={animal.name} />
     <h1>{animal.name}</h1>
     <p>{animal.shortDescription}</p>
+    </li>
     </>
 }
