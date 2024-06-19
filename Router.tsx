cf314be994296e89445692e6db68ba7d3636b05e
Andreas Retzius <andreasretzius@gmail.com>
2024-06-19 10:34:40 +0200

    🚧 Animal Details routing

--- Router.tsx
+++ Router.tsx
@@ -4,21 +4,28 @@ import { Home } from "./src/pages/Home"
 import { Animals } from "./src/pages/Animals"
 import { NotFound } from "./src/pages/NotFound";
 import { About } from "./src/pages/About";
+import { AnimalsDetailView } from "./src/pages/AnimalDetailView"
 
 export const router = createBrowserRouter([
-    {path:"/",
-        element:<Layout></Layout>,
-        children:[
-            {path:"/",
-                element:<Home></Home>
+    {
+        path: "/",
+        element: <Layout></Layout>,
+        children: [
+            {
+                path: "/",
+                element: <Home></Home>
             },
             {
-                path:"/animals",
-                element:<Animals></Animals>
+                path: "/animals",
+                element: <Animals></Animals>
             },
             {
-                path:"/about",
-                element:<About></About>
+                path: "/animalDetails",
+                element: <AnimalsDetailView></AnimalsDetailView>
+            },
+            {
+                path: "/about",
+                element: <About></About>
             }
         ],
         errorElement: <NotFound></NotFound>,
