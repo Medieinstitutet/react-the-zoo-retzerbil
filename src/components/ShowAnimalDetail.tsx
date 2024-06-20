80279932bc8c344dd2f49f601e9cf755e972f7c8
Andreas Retzius <andreasretzius@gmail.com>
2024-06-20 12:54:42 +0200

    ✨ You can now feed animals :)

--- src/components/ShowAnimalDetail.tsx
+++ src/components/ShowAnimalDetail.tsx
@@ -1,9 +1,10 @@
+import { useState } from "react";
 import { IAnimal } from "../models/IAnimal"
 interface IDetailedAnimalProps {
     currentAnimal: IAnimal
 }
 
-const handleClick = (animal: IAnimal) => {
+const handleClick = (animal: IAnimal, setLastFed:React.Dispatch<React.SetStateAction<string>>) => {
     const currentDate = new Date();
     const currentDate8601 = currentDate.toISOString();
 
@@ -12,17 +13,18 @@ const handleClick = (animal: IAnimal) => {
     if (animalToChange)  { 
             animalToChange.lastFed = currentDate8601;
             localStorage.setItem("localStorageAnimals", JSON.stringify(animals))
+            setLastFed(currentDate8601);
         };
 }
 
 export const ShowAnimalDetail = ({ currentAnimal }: IDetailedAnimalProps) => {
-
+const [lastFed, setLastFed] = useState(currentAnimal.lastFed)
     return <>
         <h1>Details for {currentAnimal.name}</h1>
         <img src={currentAnimal.imageUrl} alt={currentAnimal.name} />
         <h2>Latin name: {currentAnimal.latinName}</h2>
         <p>{currentAnimal.longDescription}</p>
-        <p>Last fed: {currentAnimal.lastFed}</p>
-        <button onClick={() => handleClick(currentAnimal)}>Feed</button>
+        <p>Last fed: {lastFed}</p>
+        <button onClick={() => handleClick(currentAnimal, setLastFed)}>Feed</button>
     </>
 }
